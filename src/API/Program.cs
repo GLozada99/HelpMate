140bec436a1a180f9d0db38402e390195a1a841a
Gustavo Lozada <gu.lozada9@gmail.com>
2025-11-20 15:24:58 -0400

    Use startup extension methods in API Program.cs

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,6 +1,26 @@
+using Infrastructure.Startup;
+using LubricambioBackend.Infrastructure.Startup;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.ConfigureDependencies();
+builder.Services.ConfigureSettings();
+builder.Services.ConfigureDatabase();
+
+builder.Host.ConfigureSerilog();
+builder.Logging.ConfigureLogging();
+
+
+builder.Services.ConfigureWeb();
+builder.Services.ConfigureApi();
+builder.Services.ConfigureCookies();
+builder.Services.ConfigureCoors();
+builder.Services.ConfigurePolicies();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.ConfigureWebApp();
+app.ConfigureSwagger();
+
 
 app.Run();
