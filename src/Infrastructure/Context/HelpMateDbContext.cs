7510c7b58b45dc38d29e7363b1c2ea7aa1b0d1e7
Gustavo Lozada <gu.lozada9@gmail.com>
2025-11-20 18:25:40 -0400

    Make values in TicketHistory not required

--- src/Infrastructure/Context/HelpMateDbContext.cs
+++ src/Infrastructure/Context/HelpMateDbContext.cs
@@ -3,6 +3,7 @@ using Domain.Entities.Board;
 using Domain.Entities.Ticket;
 using Domain.Entities.User;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace Infrastructure.Context;
 
@@ -80,7 +81,14 @@ public class HelpMateDbContext(DbContextOptions<HelpMateDbContext> options) : Db
                 v => v.Select(r => r.ToString()).ToArray(),
                 v => v.Select(Enum.Parse<BoardMembership.MembershipRoles>).ToList()
             )
-            .HasColumnType("text[]");
+            .HasColumnType("text[]")
+            .Metadata
+            .SetValueComparer(new ValueComparer<List<BoardMembership.MembershipRoles>>(
+                (l1, l2) =>
+                    (l1 != null && l2 != null && l1.SequenceEqual(l2)) ||
+                     (l1 == null && l2 == null),
+                l => l.GetHashCode()
+                ));
 
         modelBuilder.Entity<Ticket>()
             .HasOne(t => t.CreatedBy)
