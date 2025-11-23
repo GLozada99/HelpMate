74c61d4b8c972fffeece388b3e522aae3062d0b5
Gustavo Lozada <gu.lozada9@gmail.com>
2025-11-23 18:06:09 -0400

    Define UserRulesHelper

--- src/Application/Errors/User.cs
+++ src/Application/Errors/User.cs
@@ -8,6 +8,10 @@ public class UserNotFoundError(int id)
 public class UserEmailAlreadyInUseError(string email)
     : Error($"Email '{email}' is already in use.");
 
+public class InsufficientUserRoleError(string role, string action)
+    : Error(
+        $"User with role '{role}' does not have the necessary permissions for this action: {action}.");
+
 public class InsufficientUserPermissionsError(int id, string action)
     : Error(
         $"User '{id}' does not have the necessary permissions for this action: {action}.");
---
+++ src/Application/Helpers/User/UserRulesHelper.cs
@@ -0,0 +1,80 @@
+using Application.Errors;
+using Domain.Enums;
+using FluentResults;
+
+namespace Application.Helpers.User;
+
+public static class UserRulesHelper
+{
+    public static Result CanCreateUser(UserRole requesterRole, UserRole newUserRole)
+    {
+        return (requesterRole, newUserRole) switch
+        {
+            (UserRole.SuperAdmin, _) => Result.Ok(),
+
+            (UserRole.Admin, var role) when role != UserRole.SuperAdmin => Result.Ok(),
+
+            _ => Result.Fail(
+                new InsufficientUserRoleError(
+                    requesterRole.ToString(),
+                    $"Create '{newUserRole}' User"))
+        };
+    }
+
+    public static Result CanUpdateUser(UserRole requesterRole, UserRole targetUserRole)
+    {
+        return (requesterRole, targetUserRole) switch
+        {
+            (UserRole.SuperAdmin, _) => Result.Ok(),
+
+            (UserRole.Admin, var role) when role != UserRole.SuperAdmin => Result.Ok(),
+
+            _ => Result.Fail(
+                new InsufficientUserRoleError(
+                    requesterRole.ToString(),
+                    $"Update '{targetUserRole}' User"))
+        };
+    }
+
+    public static Result CanUpdateUserRole(
+        UserRole requesterRole,
+        UserRole currentUserRole,
+        UserRole newUserRole)
+    {
+        return (requesterRole, currentUserRole, newUserRole) switch
+        {
+            (UserRole.SuperAdmin, _, _) => Result.Ok(),
+
+            (UserRole.Admin, UserRole.SuperAdmin, _) =>
+                Result.Fail(
+                    new InsufficientUserRoleError("Admin", "Modify SuperAdmin")),
+
+            (UserRole.Admin, _, UserRole.SuperAdmin) =>
+                Result.Fail(
+                    new InsufficientUserRoleError("Admin", "Assign SuperAdmin role")),
+
+            (UserRole.Admin, _, _) => Result.Ok(),
+
+            _ => Result.Fail(
+                new InsufficientUserRoleError(
+                    requesterRole.ToString(),
+                    $"Update role for '{currentUserRole}' User"))
+        };
+    }
+
+    public static Result CanDeactivateUser(UserRole requesterRole,
+        UserRole targetUserRole)
+    {
+        return (requesterRole, targetUserRole) switch
+        {
+            (UserRole.SuperAdmin, _) => Result.Ok(),
+
+            (UserRole.Admin, var role) when role != UserRole.SuperAdmin => Result.Ok(),
+
+            _ => Result.Fail(
+                new InsufficientUserRoleError(
+                    requesterRole.ToString(),
+                    $"Deactivate '{targetUserRole}' User"))
+        };
+    }
+}
