7b5be3432df5fedb8a80a8c26cdd7206ff2c0780
Gustavo Lozada <gu.lozada9@gmail.com>
2025-11-24 17:34:47 -0400

    Define ITicketService

---
+++ src/Application/Interfaces/Ticket/ITicketService.cs
@@ -0,0 +1,17 @@
+using Application.DTOs.Tickets;
+using FluentResults;
+
+namespace Application.Interfaces.Ticket;
+
+public interface ITicketService
+{
+    Task<Result<TicketDTO>> CreateTicket(int boardId, CreateTicketDTO dto, int requesterId);
+
+    Task<Result<TicketDTO>> GetTicket(int boardId, int ticketId, int requesterId);
+
+    Task<Result<IQueryable<TicketListDTO>>> GetTickets(int boardId, int requesterId);
+
+    Task<Result<TicketDTO>> UpdateTicket(int boardId, int ticketId, UpdateTicketDTO dto, int requesterId);
+
+    Task<Result> DeleteTicket(int boardId, int ticketId, int requesterId);
+}
