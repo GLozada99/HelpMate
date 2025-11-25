7e7423d0f2775adcec0c3edfc6d5eb1deb1c6a74
Gustavo Lozada <gu.lozada9@gmail.com>
2025-11-24 22:40:17 -0400

    Separate membership from boards in Controllers

--- src/API/Controllers/BoardController.cs
+++ src/API/Controllers/BoardController.cs
@@ -1,6 +1,5 @@
 using API.Interfaces.Response;
 using Application.DTOs.Board.Board;
-using Application.DTOs.Board.BoardMembership;
 using Application.DTOs.Pagination;
 using Application.DTOs.Shared;
 using Application.Errors;
@@ -86,7 +85,7 @@ public class BoardController(
     }
 
     [Authorize(Roles = "Admin,SuperAdmin")]
-    [HttpPut("{id:int}")]
+    [HttpPatch("{id:int}")]
     public async Task<ActionResult<ApiResponse<BoardDTO>>> UpdateBoard(
         int id, UpdateBoardDTO dto)
     {
@@ -124,110 +123,6 @@ public class BoardController(
             BoardNotFoundError => ApiResponseHelper.Failure(errors, NotFound),
             InsufficientUserMembershipError => ApiResponseHelper.Failure(errors,
                 response => new ObjectResult(response) { StatusCode = 403 }),
-            _ => ApiResponseHelper.Failure(errors, BadRequest)
-        };
-    }
-
-    [Authorize(Roles = "Admin,SuperAdmin")]
-    [HttpPost("{boardId:int}/memberships")]
-    public async Task<ActionResult<ApiResponse<BoardMembershipDTO>>>
-        CreateBoardMembership(
-            int boardId, CreateBoardMembershipDTO dto)
-    {
-        var result =
-            await boardService.CreateBoardMembership(boardId, dto, UserId);
-
-        if (result.IsSuccess)
-            return ApiResponseHelper.Success(result.Value,
-                data => CreatedAtAction(nameof(GetBoardMemberships),
-                    new { boardId }, data));
-
-        var error = result.Errors[0];
-        var errors = result.Errors.Select(e => e.Message).ToList();
-
-        return error switch
-        {
-            UserNotFoundError or BoardNotFoundError => ApiResponseHelper.Failure(errors,
-                BadRequest),
-            BoardMembershipAlreadyExistsError => ApiResponseHelper.Failure(errors,
-                Conflict),
-            InsufficientUserMembershipError => ApiResponseHelper.Failure(errors,
-                response => new ObjectResult(response) { StatusCode = 403 }),
-            _ => ApiResponseHelper.Failure(errors, BadRequest)
-        };
-    }
-
-    [Authorize]
-    [HttpGet("{boardId:int}/memberships")]
-    public async Task<ActionResult<ApiResponse<PaginatedResponse<BoardMembershipDTO>>>>
-        GetBoardMemberships(
-            int boardId, [FromQuery] PaginationQuery paginationDto
-        )
-    {
-        var result = await boardService.GetBoardMemberships(boardId, UserId);
-
-        if (!result.IsSuccess)
-            return ApiResponseHelper.Failure(
-                result.Errors.Select(e => e.Message).ToList(),
-                BadRequest
-            );
-
-        var paginated =
-            PaginationHelper.GetPaginatedResult(result.Value, paginationDto);
-        return ApiResponseHelper.Success(paginated, Ok);
-    }
-
-    [Authorize(Roles = "Admin,SuperAdmin")]
-    [HttpPut("{boardId:int}/memberships/{userId:int}")]
-    public async Task<ActionResult<ApiResponse<BoardMembershipDTO>>>
-        UpdateBoardMembership(
-            int boardId, int userId, UpdateBoardMembershipDTO dto)
-    {
-        var result =
-            await boardService.UpdateBoardMembership(boardId, userId, dto, UserId);
-
-        if (result.IsSuccess)
-            return ApiResponseHelper.Success(result.Value, Ok);
-
-        var error = result.Errors[0];
-        var errors = result.Errors.Select(e => e.Message).ToList();
-
-        return error switch
-        {
-            UserNotFoundError => ApiResponseHelper.Failure(errors,
-                BadRequest),
-            BoardMembershipNotFoundError => ApiResponseHelper
-                .Failure(errors, NotFound),
-            InsufficientUserMembershipError or InsufficientUserPermissionsError =>
-                ApiResponseHelper.Failure(errors,
-                    response => new ObjectResult(response) { StatusCode = 403 }),
-            _ => ApiResponseHelper.Failure(errors, BadRequest)
-        };
-    }
-
-    [Authorize(Roles = "Admin,SuperAdmin")]
-    [HttpDelete("{boardId:int}/memberships/{userId:int}")]
-    public async Task<ActionResult<ApiResponse<object>>> DeleteBoardMembership(
-        int boardId, int userId)
-    {
-        var result =
-            await boardService.DeleteBoardMembership(boardId, userId, UserId);
-
-        if (result.IsSuccess)
-            return ApiResponseHelper.Success<object>(null, Ok);
-
-        var error = result.Errors[0];
-        var errors = result.Errors.Select(e => e.Message).ToList();
-
-        return error switch
-        {
-            UserNotFoundError => ApiResponseHelper.Failure(errors,
-                BadRequest),
-            BoardMembershipNotFoundError => ApiResponseHelper
-                .Failure(errors, NotFound),
-            InsufficientUserMembershipError or InsufficientUserPermissionsError =>
-                ApiResponseHelper.Failure(errors,
-                    response => new ObjectResult(response) { StatusCode = 403 }),
             _ => ApiResponseHelper.Failure(errors, BadRequest)
         };
     }
